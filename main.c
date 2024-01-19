0adec3a8d05309d23a768c7ae2c7ef10b79f33ff
Aditya Ghosh <adityaghosh5025@gmail.com>
2024-01-19 17:58:46 +0530

    added basic skeleton for bfs

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 1024 * 128
+#define MAX 512
 
 // Adds a new item to a dynamic array, resizing automatically
 // The dynamic array struct must have the fields data, size, and capacity
@@ -11,7 +13,7 @@
     if ((da)->size == (da)->capacity) {                                        \
       (da)->capacity = ((da)->capacity <= 0) ? 1 : (da)->capacity * 2;         \
       (da)->data = realloc((da)->data, (da)->capacity * sizeof(*(da)->data));  \
-      assert((da)->data && "Error in allocating");                             \
+      assert((da)->data && "Catastrophic Failure: Allocation failed!");        \
     }                                                                          \
     (da)->data[(da)->size++] = item;                                           \
   } while (0)
@@ -23,21 +25,10 @@ typedef struct {
   size_t capacity;
 } Vertices;
 
-// Adjacency list of vertices
 typedef struct {
   Vertices v[MAX];
   size_t count;
 } Graph;
-
-typedef struct {
-  Vertices v[MAX];
-  size_t count;
-} G1;
-
-typedef struct {
-  size_t count;
-  Vertices v[MAX];
-} G2;
 
 Graph graph_init() { return (Graph){0}; }
 
@@ -45,6 +36,39 @@ void graph_add_vertex(Graph *graph, int vertex) {
   assert(graph->count <= MAX && "Maximum vertices reached!");
   DYN_ADD(&graph->v[graph->count], vertex);
   graph->count++;
+}
+
+typedef int Set;
+typedef int Queue;
+
+bool que_empty(Queue *) { assert(0 && "TODO"); }
+bool que_push(Queue *, size_t) { assert(0 && "TODO"); }
+bool que_pop(Queue *) { assert(0 && "TODO"); }
+bool set_get(Set *, size_t) { assert(0 && "TODO"); }
+
+// Perform Breadth First Search
+void bfs(Graph *graph) {
+  Set visited = {MAX};
+  Queue vertices = {MAX};
+
+  // deal with disconnected vertices later on
+  // assume all graphs start with a vertex of ID: 0 for now
+  que_push(&vertices, 0);
+
+  while (!que_empty(&vertices)) {
+    size_t vertex_id = que_pop(&vertices);
+
+    // skip if vertex is already visited
+    if (set_get(&visited, vertex_id)) {
+      continue;
+    }
+    printf("%zu ", vertex_id);
+
+    Vertices children = graph->v[vertex_id];
+    for (size_t i = 0; i < children.size; i++) {
+      que_push(&vertices, children.data[i]);
+    }
+  }
 }
 
 int main() {
@@ -55,9 +79,5 @@ int main() {
   graph_add_vertex(&g, 23);
   printf("%d\n", g.v[0].data[0]);
 
-  G1 a = {0};
-  G2 b = {0};
-
-  printf("G1: %zu, G2: %zu", sizeof a, sizeof b);
   return 0;
 }
